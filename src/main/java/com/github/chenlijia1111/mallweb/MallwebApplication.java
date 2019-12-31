b0c7d8197ff81277779ac57b1c4d8cd101398e45
chenlijia <571740367@qq.com>
2019-12-31 13:05:18 +0800

    init

---
+++ src/main/java/com/github/chenlijia1111/mallweb/MallwebApplication.java
@@ -0,0 +1,13 @@
+package com.github.chenlijia1111.mallweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/chenlijia1111/mallweb/MallwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chenlijia1111.mallweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
